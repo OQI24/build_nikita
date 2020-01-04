e74921461f531806254757c950ee77242c33d313
Ivan Sukhachev <sukhachev@elcode.ru>
2020-01-04 09:44:38 +0300

    add GalleryObjects

---
+++ src/App.tsx
@@ -0,0 +1,30 @@
+import React from 'react';
+// @ts-ignore
+import {BrowserRouter, Route} from 'react-router-dom';
+import './App.css';
+import MainPage from "./containers/MainPage";
+import Header from "./containers/Header";
+import Contacts from "./containers/ContactPage";
+import Objects from "./containers/Objects";
+
+const style = require('./App.css');
+
+function App() {
+    return (
+        <BrowserRouter>
+            <div className={'App'}>
+                <Header/>
+                <div className={"app-wrapper"}>
+                    <Route path='/mainpage' component={MainPage}/>
+                    <Route path='/contacts' component={Contacts}/>
+                    <Route path='/objects' component={Objects}/>
+                </div>
+                <footer className={'Footer'}>
+                    <span className={"signature"}>by Sukhachev</span>
+                </footer>
+            </div>
+        </BrowserRouter>
+    );
+}
+
+export default App;
